bf681e15d39aa0224cdba37dc9438a36303eee4a
Kin <winterheat@gmail.com>
2011-04-10 21:44:24 -0700

    created by    rails new foo

---
+++ foo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foo::Application.config.secret_token = '464a22cc8da1459cabe16c4e73776190922c8a61f9efd4a0f8aec9708f418caccc6a64bed151efb762cb09b90db509f39e63eaa593b666811edf5620265de8ff'
